d9a40f35f2a5d670ebd28f5c150f6c3873922e16
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2022-08-03 11:07:04 -0400

    - fixing python2 -> 3 print - making executable - changing interpreter from 2 -> 3

--- nessus_merger.py
+++ nessus_merger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # derived from https://gist.github.com/mastahyeti/2720173
 # Extended to allow a custom directory to be specified via command line argument.
@@ -10,27 +10,27 @@ def main():
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hd:v", ["help", "dir="])
 	except getopt.GetoptError as err:
-		print str(err)
-		print 'nessus_merger.py -d <target_directory>'
+		print(str(err))
+		print('nessus_merger.py -d <target_directory>')
 		sys.exit(2)
 	dir = None
 	verbose = False
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
-			print 'nessus_merger.py -d <target_directory>'
+			print('nessus_merger.py -d <target_directory>')
 			sys.exit()
 		elif opt in ("-d", "--dir"):
 			dir = arg
 		else:
 			assert False, "unhandled option"
-	print 'Searching for .nessus files to merge in directory: ', dir
+	print('Searching for .nessus files to merge in directory: ', dir)
 
 	### Starting important stuff
 
 	firstFileParsed = True
 	for fileName in os.listdir(dir):
 		if ".nessus" in fileName:
-			print "Parsing - " + dir + fileName
+			print("Parsing - " + dir + fileName)
 			if firstFileParsed:
 				mainTree = etree.parse(dir + fileName)
 				report = mainTree.find('Report')
@@ -41,14 +41,14 @@ def main():
 				for host in tree.findall('.//ReportHost'):
 					existing_host = report.find(".//ReportHost[@name='"+host.attrib['name']+"']")
 					if not existing_host:
-						print "adding host: " + host.attrib['name']
+						print("adding host: " + host.attrib['name'])
 						report.append(host)
 					else:
 						for item in host.findall('ReportItem'):
 							if not existing_host.find("ReportItem[@port='"+ item.attrib['port'] +"'][@pluginID='"+ item.attrib['pluginID'] +"']"):
-								print "adding finding: " + item.attrib['port'] + ":" + item.attrib['pluginID']
+								print("adding finding: " + item.attrib['port'] + ":" + item.attrib['pluginID'])
 								existing_host.append(item)
-			print " => done."
+			print(" => done.")
 
 	if "nss_report" in os.listdir("."):
 		shutil.rmtree("nss_report")
